5124d3f8ba8a6eacd2720c375a04f53367603004
WSTym <serial.0.killer@gmail.com>
2020-03-15 17:42:11 -0300

    App finalizado

--- Afazeres/Afazeres/Afazeres/MainPage.xaml.cs
+++ Afazeres/Afazeres/Afazeres/MainPage.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Afazeres.Models;
+using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Afazeres
@@ -13,9 +11,61 @@ namespace Afazeres
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        ObservableCollection<Todo> _taskList = new ObservableCollection<Todo>();
+        Color _color;
         public MainPage()
         {
             InitializeComponent();
+            lvTask.ItemsSource = _taskList;
+
+            
         }
+
+        private void AddNewTask(Object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(taskName.Text))
+            {
+                DisplayAlert("Atenção!", "Digite uma tarefa", "Ok");
+            }
+            else if (picker.SelectedIndex < 0)
+            {
+                DisplayAlert("Atenção!", "Escolha uma prioridade", "Ok");
+            }
+            else
+            {
+                int _index = picker.SelectedIndex;
+
+                switch (_index)
+                {
+                    case 0:
+                        _color = Color.Green;
+                        break;
+                    case 1:
+                        _color = Color.Yellow;
+                        break;
+                    case 2:
+                        _color = Color.Red;
+                        break;
+                }
+
+                Todo todo = new Todo()
+                {
+                    name = taskName.Text,
+                    priority = _color
+
+                };
+
+                _taskList.Add(todo);
+            }
+            
+        }
+
+        private void OnCheckedChanged(Object sender, EventArgs e)
+        {
+            Todo todo = (Todo)((CheckBox)sender).BindingContext;
+            _taskList.Remove(todo);
+        }
+
+
     }
 }
