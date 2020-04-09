9f18395e03f859822559209f849e5e8164f823cf
WSTym <serial.0.killer@gmail.com>
2020-04-08 23:36:11 -0300

    Primeiras configurações

--- Bolao_Lotofacil/Bolao_Lotofacil/App.xaml.cs
+++ Bolao_Lotofacil/Bolao_Lotofacil/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bolao_Lotofacil.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
---
+++ Bolao_Lotofacil/Bolao_Lotofacil/Models/Aposta.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bolao_Lotofacil.Models
+{
+    [Table("Apostas")]
+     public class Aposta
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public List<int> Numeros { get; set; }
+    }
+}
